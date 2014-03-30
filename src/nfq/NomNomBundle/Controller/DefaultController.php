8f62263530875d20490408f06a0cc0c0ef67ccfa
Andrius <paulauskas_andrius@yahoo.com>
2014-03-30 15:03:27 +0300

    added avatar image serving action and improved userProfile fixture

--- src/nfq/NomNomBundle/Controller/DefaultController.php
+++ src/nfq/NomNomBundle/Controller/DefaultController.php
@@ -3,11 +3,29 @@
 namespace Nfq\NomNomBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('NfqNomNomBundle:Default:index.html.twig');
+    }
+
+    public function getAvatarAction($avatarId)
+    {
+        $userProfile = $this->getDoctrine()
+            ->getRepository("NfqNomNomBundle:myUserProfile")
+            ->find($avatarId);
+
+        $img =  base64_decode(stream_get_contents($userProfile->getAvatar()));
+
+        $response  = new Response(
+        $img,
+        Response::HTTP_OK,
+        array('content-type' => 'image/png',
+            'Content-transfer-encoding' => 'binary')
+        );
+        return $response;
     }
 }
