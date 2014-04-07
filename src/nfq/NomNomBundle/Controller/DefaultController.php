96da1316da030e046be322a68433f80bfb683f3a
sandauqo <sandauqo@yahoo.com>
2014-04-07 04:17:59 +0300

    Added profile, profile editing and password changing functionality

--- src/nfq/NomNomBundle/Controller/DefaultController.php
+++ src/nfq/NomNomBundle/Controller/DefaultController.php
@@ -13,6 +13,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\DateTime;
 
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -46,7 +56,7 @@ class DefaultController extends Controller
             $myEvent = $em->getRepository('NfqNomNomBundle:MyEvent')->find($eventId);
             if (Utilities::hasUserPermissionToEvent($myEvent, $user, $em)) {
                 $myEventArray = array();
-                 $userNames = array();
+                $userNames = array();
 
                 $myEventArray['dateCreated'] = $myEvent->getDateCreated()->format('Y-m-d H:i:s');
                 $myEventArray['eventName'] = $myEvent->getEventName();
@@ -166,7 +176,7 @@ class DefaultController extends Controller
                 $em->persist($userEvent);
                 $em->flush();
 
-                return $this->redirect($this->generateUrl('Nfq_nom_nom_events',array('eventId' => $eventId)));
+                return $this->redirect($this->generateUrl('Nfq_nom_nom_events', array('eventId' => $eventId)));
             } else {
                 return $this->render('NfqNomNomBundle:Default:adduserstoevent.html.twig',
                     array('forma' => $form->createView(),
@@ -176,4 +186,115 @@ class DefaultController extends Controller
             return $this->render('NfqNomNomBundle:Default:index.html.twig', array('error' => 'log in  first'));
         }
     }
+
+    public function profileAction()
+    {
+        return $this->render('NfqNomNomBundle:Default:profile.html.twig');
+    }
+
+    public function profileEditAction(Request $request)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.profile.form.factory');
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+                $userManager = $this->container->get('fos_user.user_manager');
+
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->container->get('router')->generate('Nfq_nom_nom_profile');
+                    $response = new RedirectResponse($url);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+        }
+        return $this->container->get('templating')->renderResponse(
+            //'FOSUserBundle:Profile:edit.html.' . $this->container->getParameter('fos_user.template.engine'),
+            'NfqNomNomBundle:Default:profileEdit.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    public function changePasswordAction(Request $request)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::CHANGE_PASSWORD_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.change_password.form.factory');
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+                $userManager = $this->container->get('fos_user.user_manager');
+
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::CHANGE_PASSWORD_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->container->get('router')->generate('Nfq_nom_nom_profile');
+                    $response = new RedirectResponse($url);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::CHANGE_PASSWORD_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            //'FOSUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
+            'NfqNomNomBundle:Default:changepassword.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+
 }
