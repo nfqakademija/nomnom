a553bb5db65fb5b4204c5f3bb0a52ff2dba1accc
Andrius <paulauskas_andrius@yahoo.com>
2014-04-08 17:57:24 +0300

    updating. new recipe categories fixtures

--- src/nfq/NomNomBundle/DataFixtures/ORM/LoadMyRecipeCategory.php
+++ src/nfq/NomNomBundle/DataFixtures/ORM/LoadMyRecipeCategory.php
@@ -23,17 +23,22 @@ class LoadMyRecipeCategory extends AbstractFixture implements OrderedFixtureInte
     function load(ObjectManager $manager)
     {
         $category1 = new MyRecipeCategory();
-        $category1->setCategoryName('Salads');
+        $category1->setCategoryName('Side dish');
 
         $category2 = new MyRecipeCategory();
         $category2->setCategoryName('Main dish');
 
+        $category3 = new MyRecipeCategory();
+        $category2->setCategoryName('Deserts');
+
         $manager->persist($category1);
         $manager->persist($category2);
+        $manager->persist($category3);
         $manager->flush();
 
         $this->addReference('Salad',$category1);
         $this->addReference('MainDish',$category2);
+        $this->addReference('Deserts', $category3);
     }
 
     /**
