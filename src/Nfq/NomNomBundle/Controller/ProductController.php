20386866df9aeb3124a348d52caa4d72e32d14e3
Andrius <paulauskas_andrius@yahoo.com>
2014-04-14 12:56:32 +0300

    added i'll bring button with proper route and function call, but the function doesn't persis data yet

---
+++ src/Nfq/NomNomBundle/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrius
+ * Date: 4/14/14
+ * Time: 12:42 PM
+ */
+
+namespace Nfq\NomNomBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller{
+
+   public function addUserProductAction($userEventId, $recipeProductId)
+   {
+       return $this->redirect($this->generateUrl('Nfq_nom_nom_events',
+           array('eventId' => $userEventId)));
+   }
+} 
