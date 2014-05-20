d84d99c57ff64c21336bb8ffe1efa5b5210cbdb6
sandauqo <sandauqo@yahoo.com>
2014-05-20 11:52:27 +0300

    upper-lower letter fix nad css

--- src/Nfq/NomNomBundle/Form/Type/CreateProductType.php
+++ src/Nfq/NomNomBundle/Form/Type/CreateProductType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class CreateproductType extends AbstractType
+class CreateProductType extends AbstractType
 {
 
     /**
